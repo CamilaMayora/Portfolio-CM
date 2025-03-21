210a5865f0fdbc41be7dd4cc03ba9977f5c7b8ca
Camila Mayora <camilamayora04@gmail.com>
2025-03-20 23:19:42 -0300

    uno

---
+++ mayora/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Barra from './Navbar/Navbar'
+import ItemListContainer from './ItemListContainer/ItemListContainer'
+
+function App() {
+
+  return (
+    <>
+    <Barra></Barra>
+    <ItemListContainer></ItemListContainer>
+    </>
+  )
+}
+
+export default App
---
+++ mayora/src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+const ItemListContainer = () => {
+    return(
+        <div>
+            <h1>HOLA</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
